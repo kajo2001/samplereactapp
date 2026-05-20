1043655c7f54412dccc4f59b609ca7a4e1e85409
kajo2001 <kajalbhalerao75@gmail.com>
2026-05-20 13:37:07 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Addition } from './AdditionComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Addition />
   </React.StrictMode>
 );
 
